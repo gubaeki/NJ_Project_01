6a6b41827d787c05c59b228d666e7205c48fbc4d
gubaeki <110735099+gubaeki@users.noreply.github.com>
2022-08-07 19:31:03 +0900

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+function pick() {
+    var lotto = [];
+    while (lotto.length < 6) {
+        var num = parseInt(Math.random() * 45 + 1);
+        if (lotto.indexOf(num) == -1) {
+            lotto.push(num);
+        }
+    }
+    lotto.sort((a,b)=>a-b);
+}
+
